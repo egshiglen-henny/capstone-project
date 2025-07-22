10ea4fe7e5f460e5c8f0db2993aff106701d41d0
Egshiglen Enkhbayar <2024359@student.cct.ie>
2025-07-22 23:48:19 +0100

    fix: update book endpoint URL name for reverse lookup in tests

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,6 @@ app_name = 'api'
 urlpatterns = [
     # BookView.as_view() handles /books/ - create and list
     # BookDetailView.as_view() handles /books/1/ - get, put, delete
-    re_path(r"^books/$", BookView.as_view(), name='book-list-create'),
+    re_path(r"^books/$", BookView.as_view(), name='books'),
     re_path(r"^books/(?P<pk>\d+)/$", BookDetailView.as_view(), name='book-detail'),
 ]
